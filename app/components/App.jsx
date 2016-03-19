f7f1446b94676a372a4471e5d60c47fef2b625c0
leaena <lindseyengineer@gmail.com>
2016-03-19 08:39:30 -0700

    adding and editing notes is a go and some stupid styling for luls

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,59 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addNote = this.addNote.bind(this);
+    this.editNote = this.editNote.bind(this);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
   render() {
-    return <Note />;
+    const notes = this.state.notes;
+
+    return (
+      <div className="container">
+        <button className="add-button" onClick={this.addNote}>+</button>
+        <Notes notes={notes} onEdit={this.editNote}/>
+      </div>
+    );
+  }
+  addNote() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+  editNote(id, task) {
+    if(!task.trim()){
+      return;
+    }
+
+    const notes = this.state.notes.map(note => {
+      if(note.id === id && task) {
+        note.task = task;
+      }
+      return note;
+    });
+    this.setState({notes});
   }
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,51 @@
 import React from 'react';
 
-export default () => <div>Learn something</div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderEdit = this.renderEdit.bind(this);
+    this.renderNote = this.renderNote.bind(this);
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+    if(this.state.editing) {
+      return this.renderEdit();
+    }
+
+    return this.renderNote();
+  }
+  renderEdit() {
+    return <input type="text"
+      ref={
+        (e) => e ? e.selectionStart = this.props.task.length : null
+      }
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit.bind(this)}
+      onKeyPress={this.checkEnter.bind(this)} />;
+  }
+  renderNote() {
+    return <div onClick={this.edit.bind(this)}>{this.props.task}</div>;
+  }
+  edit() {
+    this.setState({
+      editing: true
+    });
+  }
+  checkEnter(e) {
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  }
+  finishEdit(e) {
+    const value = e.target.value;
+    if(this.props.onEdit) {
+      this.props.onEdit(value);
+      this.setState({
+        editing: false
+      });
+    }
+  }
+}
